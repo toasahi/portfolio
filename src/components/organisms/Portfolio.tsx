8ced1c774cba7e3bb7acaa67c7811ec5c5c76b31
toasahi <asa1313to@gmail.com>
2022-08-24 12:17:53 +0900

    Portfolioを追加

---
+++ src/components/organisms/Portfolio.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+export const Portfolio = () => {
+  return (
+    <div className='snap-start h-screen lg:px-12 xl:px-32'>
+      <div className='flex lg:justify-between h-full tracking-[0.7rem] items-center font-title text-[#333333]'>
+        <div className='flex items-center mx-auto lg:mx-0'>
+          <div className='font-light text-2xl sm:font-bold xl:text-4xl'>
+            <p>TODA</p>
+            <p>ASAHI</p>
+          </div>
+          <div className='hidden lg:block lg:w-0.5 lg:h-96 lg:mx-12 bg-[#333333]'></div>
+          <div className='font-bold hidden sm:block lg:text-8xl lg:leading-[7.5rem]'>
+            <p>PORT</p>
+            <p>FO-</p>
+            <p>LIO</p>
+          </div>
+        </div>
+        <div className='hidden lg:block h-screen lg:w-[21rem] lg:mr-12 relative'>
+          <Image src='/people.jpg' layout='fill' objectFit='cover' alt='people'></Image>
+          <div className='absolute lg:w-96 lg:h-80 border-solid border-2 border-[#333333] opacity-60 lg:-left-6 lg:top-64'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
