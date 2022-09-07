90c908dfd6970ea1309dbbd58e30ac242a430f8e
toasahi <asa1313to@gmail.com>
2022-09-07 15:19:29 +0900

    Visible propsを追加

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
-import Twitter from '../../../public/twitter.svg';
-import Github from '../../../public/github.svg';
-import Instagram from '../../../public/instagram.svg';
+import Image from 'next/image';
+import { FC } from 'react';
 
-export const Header = () => {
+type Props = {
+  isVisible: boolean;
+};
+
+export const Header: FC<Props> = (props) => {
+  const { isVisible } = props;
   const scrollToSection = (id: string): void => {
     if (typeof window !== 'undefined') {
       const el = document.getElementById(id);
@@ -12,7 +16,8 @@ export const Header = () => {
   };
 
   return (
-    <div className='z-10 sticky top-5'>
+    <div className={isVisible ? 'block z-10 sticky top-5' : 'hidden'}>
+      {/*<div className={'block z-10 sticky top-5'}>*/}
       <nav className='flex p-2 font-title text-white justify-end font-bold list-none mr-16'>
         <li
           className='text-xl px-4 pt-0.5 cursor-pointer transition ease-in-out delay-150 hover:text-[#333333] duration-300 '
@@ -30,26 +35,38 @@ export const Header = () => {
           WORKS
         </li>
         <li
-          className=' text-xl px-4 pt-0.5 cursor-pointer'
+          className='text-xl px-4 pt-0.5 cursor-pointer'
           onClick={() => scrollToSection('contact')}
         >
           CONTACT
         </li>
-        <Link href='https://github.com/toasahi'>
-          <a className='px-3'>
-            <Github />
-          </a>
-        </Link>
-        <Link href='https://twitter.com/Dasadadana13'>
-          <a className='px-3'>
-            <Twitter />
-          </a>
-        </Link>
-        <Link href='https://www.instagram.com/todadaas.04/'>
-          <a className='px-3'>
-            <Instagram />
-          </a>
-        </Link>
+        <li className='text-xl px-3 pt-0.5 cursor-pointer'>
+          <Link href='https://github.com/toasahi'>
+            <a className='px-3 relative w-7 h-7'>
+              <Image
+                src={'/github.svg'}
+                layout='fill'
+                objectFit='cover'
+                alt='github'
+                className={'px-3'}
+              />
+            </a>
+          </Link>
+        </li>
+        <li className='text-xl px-3 pt-0.5 cursor-pointer'>
+          <Link href='https://twitter.com/Dasadadana13'>
+            <a className='px-3 relative w-7 h-7'>
+              <Image src={'/twitter.svg'} layout='fill' objectFit='cover' alt='twitter' />
+            </a>
+          </Link>
+        </li>
+        <li className='text-xl px-3 pt-0.5 cursor-pointer'>
+          <Link href='https://www.instagram.com/todadaas.04/'>
+            <a className='px-3 relative w-7 h-7'>
+              <Image src={'/instagram.svg'} layout='fill' objectFit='cover' alt='instagram' />
+            </a>
+          </Link>
+        </li>
       </nav>
     </div>
   );
