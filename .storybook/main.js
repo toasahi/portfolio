89d59a7a527d6b4c660ef1c8660ed669ca508d80
toasahi <asa1313to@gmail.com>
2022-09-07 15:18:15 +0900

    format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  staticDirs: ["../public"],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
