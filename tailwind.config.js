1cce795472e903395aa3cd982212de6e766bac67
toasahi <asa1313to@gmail.com>
2023-01-13 22:26:49 +0900

    レスポンシブを追加,浮き出るテキストのアニメーションを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,12 +20,24 @@ module.exports = {
         line: ['#333333'],
         input: ['#B6B4B0'],
       },
+      screens: {
+        sp: '428px', // スマホサイズを追加
+        maxsp: { max: '427px' }, // maxも追加
+        sptb: { min: '428px', max: '559px' }, // min-maxを追加
+
+        tb: '600px', // タブレットサイズを追加
+        maxtb: { max: '599px' },
+        tbpc: { min: '600px', max: '1023px' },
+
+        maxpc: { max: '1023px' },
+        pc: '1024px',
+      },
       keyframes: {
         slideView1: {
           from: {
             transform: 'translate(400%)',
           },
-          "50%":{
+          '50%': {
             transform: 'translate(100%)',
           },
           to: {
@@ -56,12 +68,21 @@ module.exports = {
             transform: 'translate(-200%);',
           },
         },
+        floatText: {
+          from: {
+            opacity: '0',
+          },
+          to: {
+            opacity: '1',
+          },
+        },
       },
       animation: {
         slideView1: 'slideView1 40s linear infinite;',
         slideView2: 'slideView2 50s linear infinite;',
         slideView3: 'slideView3 60s linear infinite;',
         slideView4: 'slideView4 70s linear infinite;',
+        floatText: 'floatText 0.7s cubic-bezier(0.33, 1, 0.68, 1) 1 forwards;',
       },
     },
   },
