c81c6d4075aab0c3a084daccc197a3e0eea32778
toasahi <asa1313to@gmail.com>
2023-01-16 00:54:56 +0900

    imageタグを削除,svgを直接読み込む

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
-import Image from 'next/image';
+
+import Github from '../../../public/github.svg';
+import Twitter from '../../../public/twitter.svg';
+import Instagram from '../../../public/instagram.svg';
 
 import { ReactNode } from 'react';
 
@@ -11,37 +14,31 @@ export const Layout = ({ children }: Props) => {
   return (
     <main className='h-full'>
       {children}
-      <footer className='bg-portfolioLine-0 h-24'>
+      <footer className='bg-portfolioLine-0 h-full'>
         <nav className='flex font-title justify-center font-bold list-none pt-2'>
           <li className='text-xl px-3 cursor-pointer'>
             <Link href='https://github.com/toasahi'>
-              <a className='px-3 relative w-7 h-7'>
-                <Image
-                  src={'/github.svg'}
-                  layout='fill'
-                  objectFit='cover'
-                  alt='github'
-                  className={'px-3'}
-                />
+              <a className='px-3'>
+                <Github width={32} height={32} />
               </a>
             </Link>
           </li>
           <li className='text-xl px-3  cursor-pointer'>
             <Link href='https://twitter.com/Dasadadana13'>
-              <a className='px-3 relative w-7 h-7'>
-                <Image src={'/twitter.svg'} layout='fill' objectFit='cover' alt='twitter' />
+              <a className='px-3 relative'>
+                <Twitter width={32} height={32} />
               </a>
             </Link>
           </li>
           <li className='text-xl px-3  cursor-pointer'>
             <Link href='https://www.instagram.com/todadaas.04/'>
-              <a className='px-3 relative w-7 h-7'>
-                <Image src={'/instagram.svg'} layout='fill' objectFit='cover' alt='instagram' />
+              <a className='px-3 relative'>
+                <Instagram width={32} height={32} />
               </a>
             </Link>
           </li>
         </nav>
-        <div className=' text-white text-center pt-4'>
+        <div className=' text-white text-center '>
           <small>&copy; 2023 ASAHI TODA</small>
         </div>
       </footer>
